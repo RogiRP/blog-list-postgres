b7faadefc6b802ad1b03776403282fa1720cccca
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-10 05:17:28 -0600

    13.20 endpoints readinglists y usuario individual completado

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,25 @@ router.get('/', async (req, res) => {
   res.json(users)
 })
 
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id, {
+    attributes: ['name', 'username'],
+    include: {
+      model: Blog,
+      as: 'readings',
+      attributes: { exclude: ['userId'] },
+      through: {
+        attributes: []
+      }
+    }
+  })
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
+
 router.post('/', async (req, res) => {
   const user = await User.create(req.body)
   res.json(user)
