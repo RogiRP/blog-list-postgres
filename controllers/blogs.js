1dd4607d1851b8dc2dfdf20f0421a5d1fddc088b
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-10 04:48:57 -0600

    13.13 filtrado por palabra clave en title completado

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,24 @@
 ﻿const router = require('express').Router()
+const { Op } = require('sequelize')
 
 const { Blog, User } = require('../models')
 const { tokenExtractor } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
