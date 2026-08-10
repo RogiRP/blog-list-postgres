9e8415d57384e9801f513210f2e9959c88c0aeef
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-10 15:58:56 -0600

    13.23 filtrado de readinglists por estado read completado

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,12 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+  const throughWhere = {}
+
+  if (req.query.read !== undefined) {
+    throughWhere.read = req.query.read === 'true'
+  }
+
   const user = await User.findByPk(req.params.id, {
     attributes: ['name', 'username'],
     include: {
@@ -20,7 +26,8 @@ router.get('/:id', async (req, res) => {
       as: 'readings',
       attributes: { exclude: ['userId'] },
       through: {
-        attributes: ['id', 'read']
+        attributes: ['id', 'read'],
+        where: throughWhere
       }
     }
   })
