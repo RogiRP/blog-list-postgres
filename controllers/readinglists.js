4b3a3707459342d24c175f79052b36fc1812ac8f
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-10 15:55:55 -0600

    13.22 marcar blog como leido con verificacion de propietario completado

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,12 +1,26 @@
 ﻿const router = require('express').Router()
 
 const { ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
   const reading = await ReadingList.create({
     blogId: req.body.blogId,
     userId: req.body.userId
   })
+  res.json(reading)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const reading = await ReadingList.findByPk(req.params.id)
+  if (!reading) {
+    return res.status(404).end()
+  }
+  if (reading.userId !== req.decodedToken.id) {
+    return res.status(401).json({ error: 'only the owner can mark this as read' })
+  }
+  reading.read = req.body.read
+  await reading.save()
   res.json(reading)
 })
 
