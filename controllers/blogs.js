c5b1f9ae5341bdb7ce870a1c9a6393e4f60f2792
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-09 23:49:02 -0600

    13.6 actualizar likes de un blog completado

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -24,4 +24,15 @@ router.delete('/:id', async (req, res) => {
   res.status(204).end()
 })
 
+router.put('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    blog.likes = req.body.likes
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 module.exports = router
